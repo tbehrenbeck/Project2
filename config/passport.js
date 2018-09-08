bb8ef9c5adb1b9bb6229d0fc2b585ae1691e672d
Adnan Yousef <yousef.eddy@gmail.com>
2018-09-08 12:10:40 -0500

    Auth

--- config/passport.js
+++ config/passport.js
@@ -16,7 +16,6 @@ passport.use(new LocalStrategy(
         email: email
       }
     }).then(function(dbUser) {
-      console.log(dbUser);
       // If there's no user with the given email
       if (!dbUser) {
         return done(null, false, {
@@ -24,7 +23,7 @@ passport.use(new LocalStrategy(
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.verifyPassword(password)) {
+      else if (!dbUser.validPassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
         });
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-var session = require("express-session");
-var passport = require("passport");
+var passport = require("./config/passport");
 
 var db = require("./models");
 
@@ -25,7 +24,7 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Authentication
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(require("express-session")({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
