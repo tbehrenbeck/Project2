4fbe196e4ffec61badf1d4ba67afeae69029f21b
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-13 12:03:07 -0500

    results page circles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,20 +4,19 @@ var path = require("path");
 var buildQueryURL = require("../edamam");
 var request = require("request");
 
-module.exports = function (app) {
-
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.resolve(__dirname, "../public/main.html"));
   });
 
   // Load create account page
-  app.get("/createAccount", function (req, res) {
+  app.get("/createAccount", function(req, res) {
     res.sendFile(path.resolve(__dirname, "../public/createAccount.html"));
   });
 
   // Load a user's profile
-  app.get("/profile", isAuthenticated, function (req, res) {
+  app.get("/profile", isAuthenticated, function(req, res) {
     var username = req.user.username;
     var userId = req.user.id;
 
@@ -25,29 +24,36 @@ module.exports = function (app) {
       where: {
         username: username
       },
-      include: [{
-        model: db.Favorite,
-        where: {
-          UserId: userId,
-        },
-        required: false
-      }]
-    }).then(function (data) {
+      include: [
+        {
+          model: db.Favorite,
+          where: {
+            UserId: userId
+          },
+          required: false
+        }
+      ]
+    }).then(function(data) {
       if (data == null) {
         return res.send(`Username ${username} not found`);
-      };
+      }
       var favs = data.Favorites;
       res.render("profile", { data: data, favs: favs });
     });
   });
-  
 
   // Search for a recipe, render handlebars
-  app.get("/recipeSearch/:protein/:lower/:upper/:diet", function (req, res) {
+  app.get("/recipeSearch/:protein/:lower/:upper/:diet", function(req, res) {
     if (!req.user) {
-      return res.json({success: false, message: "You are not signed in"});
+      return res.json({ success: false, message: "You are not signed in" });
     }
-    var queryURL = buildQueryURL(req.params.protein, req.params.lower, req.params.upper, "alcohol-free", req.params.diet);
+    var queryURL = buildQueryURL(
+      req.params.protein,
+      req.params.lower,
+      req.params.upper,
+      "alcohol-free",
+      req.params.diet
+    );
     var mealCount = parseInt(req.query.mealCount) || 4;
     var suggested = {
       mealCount: mealCount,
@@ -57,20 +63,20 @@ module.exports = function (app) {
       carbsPerMeal: req.user.carbs / mealCount
     };
 
-    request(queryURL, function (err, response, body) {
+    request(queryURL, function(err, response, body) {
       if (err) {
         throw err + response;
-      };
+      }
 
-      if (body[0] === "<" ) {
-        return res.json({success: false, message: "Bad API input"})
+      if (body[0] === "<") {
+        return res.json({ success: false, message: "Bad API input" });
       }
 
       var rawData = JSON.parse(body);
       var data = [];
       for (var i = 0; i < rawData.hits.length; i++) {
         var recipe = {
-          id: i+1,
+          id: i + 1,
           title: rawData.hits[i].recipe.label,
           url: rawData.hits[i].recipe.url,
           pic: rawData.hits[i].recipe.image,
@@ -91,17 +97,17 @@ module.exports = function (app) {
         recipe.jsonData = recipeString;
 
         data.push(recipe);
-      };
+      }
       res.render("results", { data: data, suggested: suggested });
     });
   });
 
-  app.get("/updateMacros", function(req,res) {
+  app.get("/updateMacros", function(req, res) {
     res.sendFile(path.resolve(__dirname, "../public/updateMacros.html"));
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
