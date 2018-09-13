3654dac13ad53a63ac72100de6cbff4ab190dc79
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-12 19:42:30 -0500

    favorite tabs

--- public/js/recipeSearch.js
+++ public/js/recipeSearch.js
@@ -1,21 +1,36 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
   // on click of "search" button
-  $("#search-btn").on("click", function (event) {
+  $("#search-btn").on("click", function(event) {
     event.preventDefault();
 
     // take data from search parameter fields
-    var protein = $("#protein-options").val().trim();
+    var protein = $("#protein-options")
+      .val()
+      .trim();
 
-    var lower = parseInt($("#min-cals").val().trim());
-    var upper = parseInt($("#max-cals").val().trim());
-    var health = $("#health-options").val().trim();
-    var diet = $("#diet-options").val().trim();
-    var mealCount = parseInt($("#meal-count").val().trim());
+    var lower = parseInt(
+      $("#min-cals")
+        .val()
+        .trim()
+    );
+    var upper = parseInt(
+      $("#max-cals")
+        .val()
+        .trim()
+    );
+    var health = $("#health-options")
+      .val()
+      .trim();
+    var diet = $("#diet-options")
+      .val()
+      .trim();
+    var mealCount = parseInt(
+      $("#meal-count")
+        .val()
+        .trim()
+    );
 
     var destUrl = `/recipeSearch/${protein}/${lower}/${upper}/${health}/${diet}`;
     window.location.replace(destUrl);
-
   });
-
 });
